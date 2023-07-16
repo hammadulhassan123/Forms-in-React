4148daa838efdada7f5693acdf7ae7afe97c0276
Hammad Ul Hassan <hammadulhassanqureshi@gmail.com>
2023-07-16 18:46:06 +0500

    "First Attempt"

--- app/page.js
+++ app/page.js
@@ -1,113 +1,83 @@
-import Image from 'next/image'
+ "use client"; // This is a client component
+import Image from 'next/image';
+import formImage from "public/form.png";
+import './globals.css';
+// import { useState } from 'react'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const formik = useFormik({
+    initialValues:{
+      name: "",
+      email: "",
+      country:"United Kingdom",
+      terms: "",
+      
+    },
+    // Validate Form
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    validationSchema: Yup.object({
+      name: Yup.string().max(20, 'Name must be 20 chars or less ').required("Name is required"),
+      email: Yup.string().email("Invalid email address").required("Email is required"), 
+      terms: Yup.array().required('Terms of service must be checked'),
+    }),
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    onSubmit: (values)=>{    
+      console.log(formik.values);
+    }
+  });
+console.log(formik.errors);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+  // const [name, setName]=useState('');
+    return (
+    <main className=" h-screen items-center flex justify-center">
+        <form onSubmit={formik.handleSubmit} className="bg-white flex rounded-lg w-1/2 font-sans" action="">
+          <div className='flex-1 text-gray-700 p-20'>
+            <h1 className='text-3xl pb-2 font-sans'>Let's Get Started 👋</h1>
+            <p className='text-gray-400 font-sans text-lg'>
+              Join our E-learning platform today and unlock over 500+ courses and digital assests ready to download.
+            </p>
+            <div className='mt-5'>
+              {/* Name */}
+               <div className='pb-4'>
+                  <label htmlFor="name" className={`block font-bold text-sm pb-2 ${ formik.touched.name && formik.errors.name? "text-red-400": ""}`} >
+                    {formik.touched.name && formik.errors.name ? formik.errors.name : "Name"}</label>
+                  <input className="border-2 border-gray-500 p-2 rounded-md w-2/3 focus:border-blue-500 focus:ring-blue-500" type="text" name='name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder="Enter Your Full Name" />
+                </div>
+               {/* Email  */}
+               <div className='pb-4'>
+                  <label htmlFor="email" className='block font-bold text-sm pb-2'>Email</label>
+                  <input className="border-2 border-gray-500 p-2 rounded-md w-2/3 focus:border-blue-500 focus:ring-blue-500" type="email" name='email' value={formik.values.email} onChange={formik.handleChange} placeholder='Enter Your Email' />
+                </div>
+                {/* Country */}
+                <div className='pb-4'>
+                  <label htmlFor="country" className='block font-bold text-sm pb-2'>Country</label>
+                  <select className="border-2 border-gray-500 p-2 rounded-md w-2/3 focus:border-blue-500 focus:ring-blue-500" name="country" value={formik.values.country} onChange={formik.handleChange} >
+                    <option>United States</option>
+                    <option>United Kingdom</option>
+                    <option>Germany</option>
+                    <option>Norway</option>
+                 </select>                 
+                </div>
+                {/* Terms And Agreement */}
+                <div className='pb-4'> 
+                  <label htmlFor="terms" className='block font-bold text-sm pb-2'>Terms Of Service</label>
+                   <div className='flex items-center gap-2'>
+                      <input type='checkbox' name='terms' value="checked" className='h-5 w-5 text-blue-500 border-2 focus:border-blue-500 focus:ring-blue-500'/>
+                      <p className='text-sm font-bold text-gray-500' onChange={formik.handleChange}>Agree with terms and services
+                     </p>
+                   </div>
+                </div>
+                <button type='submit' className='bg-blue-500 font-bold text-sm text-white py-3 mt-6 rounded-lg w-full'>Start learning Today</button>
+            </div>
+          </div>
+          <div className=' relative flex-1'>
+            <Image src={formImage} fill className='object-cover rounded-lg' alt='Form-Image'/>
+          </div>
+        </form>
     </main>
   )
 }
